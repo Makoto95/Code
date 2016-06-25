86eafb0509b3a783466fc5ee7639580d24573143
vetus-rector <rector_eeic@gmail.com>
2016-06-25 16:17:26 +0900

    repo.c modified

--- Algorithm/repo.c
+++ Algorithm/repo.c
@@ -5,39 +5,165 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <ctype.h>
 
-#define WNUM 10000
 
-char* mmapfile(int*);
+
+#define WNUM 20000
+#define TRUE 1
+#define FALSE 0
+
+int getWord(char*, int*, int*, int*);
+int getHash(char*, int, int);
+int wordcmp(char* , char*, int);
+void wordcpy(char*, char*, int);
+
+void wordcpy(char* dest, char* src, int n){
+  int i;
+  for(i=0; i<n; i++){
+    dest[i] = tolower(src[i]);
+  }
+}
+
+int wordcmp(char* s1, char* s2, int n){
+  int i;
+  for(i = 0; i<n; i++){
+    if(s1[i] != tolower(s2[i])) break;
+  }
+  if(i==n) return 0;
+  else return 1;
+}
+
+
+
+
 
 int main(){
+  char fhword[WNUM][25];
+  memset(fhword, '\0', WNUM * 25);
+  char lhword[WNUM][20];
+  memset(lhword, '\0', WNUM * 25);
+  //unsigned char hashFlag[WNUM];
+  int fhWordCounter[WNUM] = {};
+  char countFlag[WNUM] = {};
+  memset(countFlag, TRUE, WNUM);
+  int lhWordCounter[WNUM] = {};
+  //memset(hashFlag, NONE, WNUM);
+  //char buf[25];
+  int wordLen;
+  int hash;
+  int m, n;
+  int i, j;
+  //int l;
+
+  char* s = "kane! bouryoku, sex! oppai ippai akne oppai zzzzzzzzzzz\n \t . .. piyo hoge. @ wrgareg frbaerb e! oppai ippai pi";
+/*
   int filesize;
-  //char fhword[WNUM][20];
-  //char lhword[WNUM][20];
-  char buf[20];
-  char c;
-  /*int n;
-  int k;
-  int l;*/
-  int i = 0;
   int pagesize, mmapsize;
   char* addr;
-  //char s[] = "qwert e6thywrtw ryjtyje rt6hwryh wet4hw";
-  char token[] = " .\t\n";
   filesize = lseek(0, 0, SEEK_END);
   pagesize = getpagesize();
   mmapsize = (filesize + (pagesize-1)) / pagesize * pagesize;
   addr = mmap(0, mmapsize, PROT_READ, MAP_PRIVATE, 0, 0);
-  //puts(addr);
-  puts(token);
-  printf("%c\n", addr[0]);
-  while(addr[i] != ' '){
-    buf[i] = addr[i++];
+*/
+  int now = 0;
+  int start = 0;
+  int end = 0;
+  while(1){
+    wordLen = getWord(s, &now, &start, &end);
+    if(s[now] == '@' && (wordLen < 1)) break;
+    hash = getHash(s, start, end);
+
+    i = hash;
+    n = hash+10;
+    while(fhWordCounter[i] != 0 && i < n){
+      if(wordcmp(fhword[i], &s[start], wordLen) == 0){
+        fhWordCounter[i]++;
+        break;
+      }
+      i++;
+    }
+    if(fhWordCounter[i] == 0){
+      wordcpy(fhword[i], &s[start], wordLen);
+      fhWordCounter[i] = 1;
+    }
+
+    fprintf(stderr, "%s\t%d\n", fhword[i], fhWordCounter[i]);
   }
-  puts(buf);
-  /*while(buf != NULL){
-    buf = strtok(NULL, token);
-    if(buf != NULL) puts(buf);
-  }*/
+
+  now++;
+
+  while(1){
+    wordLen = getWord(s, &now, &start, &end);
+    if(s[now] == '\0' && (wordLen < 1)) break;
+    hash = getHash(s, start, end);
+
+    i = hash;
+    n = hash+10;
+    while(fhWordCounter[i] != 0 && i < n){
+      if(wordcmp(fhword[i], &s[start], wordLen) == 0){
+        countFlag[i] = FALSE;
+        break;
+      }
+      i++;
+    }
+    if(fhWordCounter[i] == 0){
+      j = hash;
+      m = hash+10;
+      while(lhWordCounter[j] != 0 && j < m){
+        if(wordcmp(lhword[j], &s[start], wordLen) == 0){
+          lhWordCounter[j]++;
+          break;
+        }
+        j++;
+      }
+      if(lhWordCounter[j] == 0){
+        wordcpy(lhword[i], &s[start], wordLen);
+        lhWordCounter[i] = 1;
+      }
+    }
+
+    fprintf(stderr, "%s\t%d\n", lhword[i], lhWordCounter[i]);
+  }
+
+  fprintf(stderr, "\nfast half\n");
+  for(i=0; i<WNUM; i++){
+    if(fhWordCounter[i] != 0 && countFlag[i] == TRUE){
+      fprintf(stderr, "%s\t%d\n", fhword[i], fhWordCounter[i]);
+    }
+  }
+  fprintf(stderr, "\nlast half\n");
+  for(i=0; i<WNUM; i++){
+    if(lhWordCounter[i] != 0){
+      fprintf(stderr, "%s\t%d\n", lhword[i], lhWordCounter[i]);
+    }
+  }
+
+
   return 0;
+}
+
+int getHash(char* a, int s, int e){
+  int hash = 0;
+  int i;
+  for(i=s; i<=e; i++){
+    hash += tolower(a[i]);
+  }
+  return hash * 10;
+}
+
+int getWord(char* addr, int* now, int* start, int* end){
+  int i = *now;
+  while(!isalnum(addr[i]) && addr[i] != '\0' && addr[i] != '@') i++;
+  *start = i;
+  while(!ispunct(addr[i]) && !isspace(addr[i]) && addr[i] != '\0' && addr[i] != '@'){
+    i++;
+  }
+  *now = i;
+  while(!isalnum(addr[i])){
+    i--;
+  }
+  *end = i;
+  return (*end)-(*start)+1;
 }
