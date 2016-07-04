84d7fcae8cda3ff424f0f5afa7ba69361740f4de
vetus-rector <rector_eeic@gmail.com>
2016-07-05 08:57:30 +0900

    OpenALplayTestEnd

--- exp/I2/day4/OpenALplay.c
+++ exp/I2/day4/OpenALplay.c
@@ -16,19 +16,22 @@ int main(int argc, char *argv[]){
   ALCdevice* device = alcOpenDevice(NULL);
   ALCcontext* context = alcCreateContext(device, NULL);
   alcMakeContextCurrent(context);
-  short data[SRATE];
+  ALshort data[SRATE*1];
+  memset(data, 0, sizeof(ALshort)*SRATE);
   ALuint buffer_id;
   ALuint source_id;
   alGenBuffers(1, &buffer_id);
   alGenSources(1, &source_id);
   int n;
   while(1){
-    n = read(0, data, SRATE*2);
+    n = read(0, data, SRATE*sizeof(ALshort)*1);
     if(n==0) break;
-    alBufferData (buffer_id, AL_FORMAT_MONO16, &data[0], sizeof(data), SRATE);
+    write(1, data, SRATE*sizeof(ALshort));
+    alBufferData(buffer_id, AL_FORMAT_MONO16, data, sizeof(ALshort)*SRATE*1, SRATE);
     alSourcei(source_id, AL_BUFFER, buffer_id);
+    alSourcei(source_id, AL_LOOPING, AL_FALSE);
     alSourcePlay(source_id);
-    sleep(1);
+    alutSleep(1);
     alSourcei(source_id, AL_BUFFER, 0);
   }
   alDeleteSources(1, &source_id);
@@ -37,4 +40,5 @@ int main(int argc, char *argv[]){
   alcDestroyContext(context);
   alcCloseDevice(device);
   alutExit();
+  return 0;
 }
